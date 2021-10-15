193e3a4ed8084914a65269d0e176d1964fbfdf62
Antonio Cabrera <antonio@abrevia.net>
2021-10-15 15:22:28 +0200

    Update 01_a_and_b.js

--- examples/01_a_and_b.js
+++ examples/01_a_and_b.js
@@ -1,20 +1,20 @@
 const { createContainer } = require("..");
 
 createContainer().
-	add("a", AFactory).
-	add("b", BFactory).
+	add("a", A).
+	add("b", B).
 	consume(({a})=>{
 		console.log("✓ Let's consume");
 		console.log("❯", a.greet("Anthony"));
 	});
 
-function AFactory({b}){
+function A({b}){
 	console.log("✓ A has been instantiated");
 	return {
 		greet: (name)=>b.say(`Hello ${name}`)
 	};
 }
-function BFactory({}){
+function B({}){
 	console.log("✓ B has been instantiated");
 	return {
 		say: (something)=>`Say: "${something}"`
